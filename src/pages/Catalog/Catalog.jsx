ba047a4c8a2926c662e50206fe8de2c55e844ded
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-09-17 14:13:56 +0300

    added modifications/load more btn

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -6,25 +6,27 @@ import CarsList from "../../components/CarsList/CarsList";
 import { fetchCars } from "../../services/apiCars";
 
 const Catalog = () => {
-  const [cars, setCars] = useState();
+  const [cars, setCars] = useState([]);
+  const [page] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const cars = await fetchCars();
+        const cars = await fetchCars(page);
 
-        setCars(cars);
+        setCars([...cars]);
       } catch (e) {
         throw e.message;
       }
     };
 
     fetchData();
-  }, []);
+  }, [page]);
   return (
     <main className={`${styles.catalog} container`}>
       <FilterForm />
       <CarsList cars={cars} />
+      <button className={styles.loadMoreBtn}>Load more</button>
     </main>
   );
 };
--- src/services/apiCars.js
+++ src/services/apiCars.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://6505e8b8ef808d3c66f09ae5.mockapi.io/api/v1";
 
-export const fetchCars = async () => {
+export const fetchCars = async (page) => {
   try {
-    const response = await axios.get("/cars");
+    const response = await axios.get(`/cars/?&page=${page}&limit=8`);
     return response.data;
   } catch (e) {
     throw e.message;
